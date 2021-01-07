63bb808303f7d679c2444b1e2f5d7298001b8538
Mary Voulieri <maryvou25@gmail.com>
2021-01-07 21:44:54 +0200

    Fixed some issues

--- gr.hua.Personnel-Applications/src/main/resources/data.sql
+++ gr.hua.Personnel-Applications/src/main/resources/data.sql
@@ -2,40 +2,75 @@ SET foreign_key_checks = 0;
 
 INSERT INTO `department`(dep_name,super_id_fk)
     select t.*
-    from ((SELECT 'Secretariat' as col1, 2 as col2) union all
-          (SELECT 'IT', 1)
+    from ((SELECT 'IT' as col1, 2 as col2) union all
+          (SELECT 'Payroll Department', 3) union all
+          (SELECT 'Human Resources', 4) union all
+          (SELECT 'Marketing', 5)
          ) t
     WHERE NOT EXISTS (SELECT * FROM `department`);
 
 INSERT INTO user(username,password, emp_id)
 	select t.*
-	from((SELECT 'MaryVou' as col1, '$2y$12$CgaJlvwYCB8kJfuk8fL68eBI2JQjxvvrXu4fpEK..WOvluvkOqPI6' as col2, 1 as col3) union all
-		(SELECT 'ElliKal', '$2y$12$CgaJlvwYCB8kJfuk8fL68eBI2JQjxvvrXu4fpEK..WOvluvkOqPI6',2) union all
-		(SELECT 'NikakiTreloGavraki', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',3)
+	from((SELECT 'Manager' as col1, '$2y$12$CgaJlvwYCB8kJfuk8fL68eBI2JQjxvvrXu4fpEK..WOvluvkOqPI6' as col2, 1 as col3) union all
+		(SELECT 'Supervisor1', '$2y$12$CgaJlvwYCB8kJfuk8fL68eBI2JQjxvvrXu4fpEK..WOvluvkOqPI6',2) union all
+		(SELECT 'Supervisor2', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',3) union all
+		(SELECT 'Supervisor3', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',4) union all
+		(SELECT 'Supervisor4', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',5) union all
+		(SELECT 'Admin', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',6) union all
+		(SELECT 'It_Employee1', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',7) union all
+		(SELECT 'It_Employee2', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',8) union all
+		(SELECT 'PD_Employee1', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',9) union all
+		(SELECT 'PD_Employee2', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',10) union all
+		(SELECT 'HR_Employee1', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',11) union all
+		(SELECT 'HR_Employee2', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',12) union all
+		(SELECT 'M_Employee1', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',13) union all
+		(SELECT 'M_Employee2', '$2y$12$Ml7Mf8OIZOPI.wynlTmz4OV6dM5Z9WaOJQELq/0kVoufTy4ni9/DC',14)
 		)t
 	WHERE NOT EXISTS(SELECT * FROM `user`);
 
 INSERT INTO authorities(username,authority)
 	select t.*
-	from((SELECT 'MaryVou' as col1, 'ROLE_ADMIN' as col2) union all
-		(SELECT 'ElliKal','ROLE_SUPERVISOR') union all
-		(SELECT 'NikakiTreloGavraki','ROLE_EMPLOYEE')
+	from((SELECT 'Manager' as col1, 'ROLE_MANAGER' as col2) union all
+		(SELECT 'Supervisor1','ROLE_SUPERVISOR') union all
+		(SELECT 'Supervisor2','ROLE_SUPERVISOR') union all
+		(SELECT 'Supervisor3','ROLE_SUPERVISOR') union all
+		(SELECT 'Supervisor4','ROLE_SUPERVISOR') union all
+		(SELECT 'Admin','ROLE_ADMIN') union all
+		(SELECT 'It_Employee1','ROLE_EMPLOYEE') union all
+		(SELECT 'It_Employee2','ROLE_EMPLOYEE') union all
+		(SELECT 'PD_Employee1','ROLE_PDEMPLOYEE') union all
+		(SELECT 'PD_Employee2','ROLE_PDEMPLOYEE') union all
+		(SELECT 'HR_Employee1','ROLE_EMPLOYEE') union all
+		(SELECT 'HR_Employee2','ROLE_EMPLOYEE') union all
+		(SELECT 'M_Employee1','ROLE_EMPLOYEE') union all
+		(SELECT 'M_Employee2','ROLE_EMPLOYEE')
 		)t
 	WHERE NOT EXISTS(SELECT * FROM `authorities`);			
 
 INSERT INTO employee(f_name,l_name,email,phone,address,birth_date,hire_date,dep_id_fk,username_fk)
 	select t.*
-	from((SELECT 'Mary' as col1,'Voulieri' as col2,'it21710@gmail.com' as col3,'6906969696' as col4,'Steriperi' as col5,'1999-05-20' as col6,'2020-12-10' as col7, 2 as col8,'MaryVou' as col9) union all
-		(SELECT 'Elli','Kalavrytinou','it21721@gmail.com','6906969669','Kifisia','1999-09-23','2020-12-10', 1, 'ElliKal') union all
-		(SELECT 'Niki','Anagnostopoulou','pada@gmail.com','6906969696','Steriperi','1999-10-28','2020-12-10', 1,'NikakiTreloGavraki')
+	from((SELECT 'Jane' as col1,'Doe' as col2,'manager@gmail.com' as col3,'6909090901' as col4,'Somewhere on the map' as col5,'1999-05-20' as col6,'2020-12-10' as col7, null as col8,'Manager' as col9) union all
+		(SELECT 'Jane','Doe','super1@gmail.com','6909090902','Somewhere on the map','1999-09-23','2020-12-10', 1, 'Supervisor1') union all
+		(SELECT 'Jane','Doe','super2@gmail.com','6909090903','Somewhere on the map','1999-09-23','2020-12-10', 2, 'Supervisor2') union all
+		(SELECT 'Jane','Doe','super3@gmail.com','6909090904','Somewhere on the map','1999-09-23','2020-12-10', 3, 'Supervisor3') union all
+		(SELECT 'Jane','Doe','super4@gmail.com','6909090905','Somewhere on the map','1999-09-23','2020-12-10', 4, 'Supervisor4') union all
+		(SELECT 'Jane','Doe','admin@gmail.com','6909090906','Somewhere on the map','1999-09-23','2020-12-10', 1, 'Admin') union all
+		(SELECT 'Jane','Doe','it1@gmail.com','6909090907','Somewhere on the map','1999-09-23','2020-12-10', 1, 'It_Employee1') union all
+		(SELECT 'Jane','Doe','it2@gmail.com','6909090908','Somewhere on the map','1999-09-23','2020-12-10', 1, 'It_Employee2') union all
+		(SELECT 'Jane','Doe','pd1@gmail.com','6909090909','Somewhere on the map','1999-09-23','2020-12-10', 2, 'PD_Employee1') union all
+		(SELECT 'Jane','Doe','pd2@gmail.com','6909090910','Somewhere on the map','1999-09-23','2020-12-10', 2, 'PD_Employee2') union all
+		(SELECT 'Jane','Doe','hr1@gmail.com','6909090911','Somewhere on the map','1999-09-23','2020-12-10', 3, 'HR_Employee1') union all
+		(SELECT 'Jane','Doe','hr2@gmail.com','6909090912','Somewhere on the map','1999-09-23','2020-12-10', 3, 'HR_Employee2') union all
+		(SELECT 'Jane','Doe','m1@gmail.com','6909090913','Somewhere on the map','1999-09-23','2020-12-10', 4, 'M_Employee1') union all
+		(SELECT 'Jane','Doe','m2@gmail.com','6909090914','Somewhere on the map','1999-09-23','2020-12-10', 4, 'M_Employee2')
 		)t
 	WHERE NOT EXISTS(SELECT * FROM `employee`);
 
 INSERT INTO application(type,days,start_date,last_date,emp_id_fk)
 	select t.*
-	from((SELECT 'recovery' as col1, 3 as col2, '2020-12-17' as col3, '2020-12-20' as col4, 1 as col5) union all
-		(SELECT 'strike',2,'2020-12-12','2020-12-14',2) union all
-		(SELECT 'student',2,'2020-12-12','2020-12-14',3)
+	from((SELECT 'recovery' as col1, 3 as col2, '2020-12-17' as col3, '2020-12-20' as col4, 12 as col5) union all
+		(SELECT 'strike',2,'2020-12-12','2020-12-14',5) union all
+		(SELECT 'student',2,'2020-12-12','2020-12-14',7)
 		)t
 	WHERE NOT EXISTS(SELECT * FROM `application`);
 
--- gr.hua.Personnel-Applications/src/main/resources/schema.sql
+++ gr.hua.Personnel-Applications/src/main/resources/schema.sql
@@ -6,53 +6,53 @@ CREATE TABLE IF NOT EXISTS `user`(
 	`enabled` tinyint DEFAULT 1,
 	`emp_id` int,
 	PRIMARY KEY(`username`),
-	CONSTRAINT `fk_user_employee` FOREIGN KEY(`emp_id`) REFERENCES `employee`(`emp_id`) ON DELETE CASCADE
+	CONSTRAINT `fk_user_employee` FOREIGN KEY(`emp_id`) REFERENCES `employee`(`emp_id`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE IF NOT EXISTS `authorities`(
   `username` varchar(50) NOT NULL UNIQUE,
   `authority` varchar(50) NOT NULL,
   UNIQUE KEY `ix_auth_username` (`username`,`authority`),
-  CONSTRAINT `fk_authorities_user` FOREIGN KEY(`username`) REFERENCES `user`(`username`) ON DELETE CASCADE
+  CONSTRAINT `fk_authorities_user` FOREIGN KEY(`username`) REFERENCES `user`(`username`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE IF NOT EXISTS `employee`(
 	`emp_id` int AUTO_INCREMENT,
-	`f_name` varchar(20),
-	`l_name` varchar(20),
-	`email` varchar(30),
-	`phone` varchar(15),
-	`address` varchar(30),
-	`birth_date` date,
-	`hire_date` date,
+	`f_name` varchar(20) NOT NULL,
+	`l_name` varchar(20) NOT NULL,
+	`email` varchar(30) NOT NULL UNIQUE,
+	`phone` varchar(15) NOT NULL UNIQUE,
+	`address` varchar(30) NOT NULL,
+	`birth_date` date NOT NULL,
+	`hire_date` date NOT NULL,
 	`dep_id_fk` int,
 	`username_fk` varchar(50),
 	PRIMARY KEY(`emp_id`),
-	CONSTRAINT `fk_employee_user` FOREIGN KEY(`username_fk`) REFERENCES `user`(`username`) ON DELETE SET NULL,
-	CONSTRAINT `fk_employee_department` FOREIGN KEY(`dep_id_fk`) REFERENCES `department`(`dep_id`) ON DELETE SET NULL
+	CONSTRAINT `fk_employee_user` FOREIGN KEY(`username_fk`) REFERENCES `user`(`username`) ON DELETE SET NULL ON UPDATE CASCADE,
+	CONSTRAINT `fk_employee_department` FOREIGN KEY(`dep_id_fk`) REFERENCES `department`(`dep_id`) ON DELETE SET NULL ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE IF NOT EXISTS `department`(
 	`dep_id` int NOT NULL AUTO_INCREMENT,
-	`dep_name` varchar(20),
+	`dep_name` varchar(20) NOT NULL UNIQUE,
 	`super_id_fk` int,
 	PRIMARY KEY(`dep_id`),
-	CONSTRAINT `fk_department_employee` FOREIGN KEY(`super_id_fk`) REFERENCES `employee`(`emp_id`) ON DELETE SET NULL
+	CONSTRAINT `fk_department_employee` FOREIGN KEY(`super_id_fk`) REFERENCES `employee`(`emp_id`) ON DELETE SET NULL ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE IF NOT EXISTS `application`(
 	`app_id` int NOT NULL AUTO_INCREMENT,
-	`type` varchar(15),
-	`days` int,
-	`start_date` date,
-	`last_date` date,
+	`type` varchar(15) NOT NULL,
+	`days` int NOT NULL,
+	`start_date` date NOT NULL,
+	`last_date` date NOT NULL,
 	`req_papers` blob,
 	`super_sig` tinyint,
 	`pd_sig` tinyint,
 	`mgr_sig` tinyint,
 	`emp_id_fk` int,
 	PRIMARY KEY(`app_id`),
-	CONSTRAINT `fk_application_employee` FOREIGN KEY(`emp_id_fk`) REFERENCES `employee`(`emp_id`) ON DELETE CASCADE
+	CONSTRAINT `fk_application_employee` FOREIGN KEY(`emp_id_fk`) REFERENCES `employee`(`emp_id`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 SET foreign_key_checks = 1;
