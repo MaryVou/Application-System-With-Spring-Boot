8cff64fc16219b97c440ba26e6f2ca79d9a09872
Mary Voulieri <maryvou25@gmail.com>
2021-01-08 00:22:49 +0200

    Updated Security

--- gr.hua.Personnel-Applications/src/main/java/gr/hua/PersonnelApplications/WebSecurityConfig.java
+++ gr.hua.Personnel-Applications/src/main/java/gr/hua/PersonnelApplications/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/**").hasAnyRole("ADMIN","MANAGER","SUPERVISOR","PDEMPLOYEE").anyRequest().authenticated().and().formLogin().permitAll().and().logout().permitAll();
 	}
 
 	@Bean
